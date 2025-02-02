c853978bc84a3f16ad137314414e80c759bd7c37
Henry Dollman <hank@henrygd.me>
2025-02-01 20:21:02 -0500

    wip: progress on serving from subpath (#33)

--- beszel/site/src/components/router.tsx
+++ beszel/site/src/components/router.tsx
@@ -1,16 +1,43 @@
 import { createRouter } from "@nanostores/router"
 
-export const $router = createRouter(
-	{
-		home: "/",
-		server: "/system/:name",
-		settings: "/settings/:name?",
-		forgot_password: "/forgot-password",
-	},
-	{ links: false }
-)
+const routes = {
+	home: "/",
+	system: `/system/:name`,
+	settings: `/settings/:name?`,
+	forgot_password: `/forgot-password`,
+} as const
 
-/** Navigate to url using router */
+// TODO: fix /static links like favicon and check login
+
+// calculate base path based on current path and position of routes
+// this is needed to support serving from subpaths like beszel.com/example-base
+export const basePath = (() => {
+	const baseRoutes = Object.values(routes).map((route) => route.split("/").at(1))
+	const pathSegments = window.location.pathname.split("/").filter(Boolean)
+	for (let i = 0; i < pathSegments.length; i++) {
+		if (baseRoutes.includes(pathSegments[i])) {
+			// If a route is found, the base path is everything before this segment
+			return "/" + pathSegments.slice(0, i).join("/")
+		}
+	}
+	return "/" + pathSegments.join("/")
+})()
+
+export const prependBasePath = (path: string) => `${basePath}${path}`.replaceAll("//", "/")
+
+// prepend base path to routes
+for (const route in routes) {
+	// @ts-ignore need as const above to get nanostores to parse types properly
+	routes[route] = prependBasePath(routes[route])
+}
+
+// console.log("routes", routes)
+
+export const $router = createRouter(routes, { links: false })
+
+/** Navigate to url using router
+ *  Base path is automatically prepended if serving from subpath
+ */
 export const navigate = (urlString: string) => {
 	$router.open(urlString)
 }
